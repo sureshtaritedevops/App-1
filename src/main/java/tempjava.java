ffdda7c2668be3f21978ef9aab5e2b78b933b7aa
sureshtaritedevops <sureshtaritedevops@gmail.com>
2017-07-01 15:43:15 +0530

    tempjava modified

--- src/main/java/tempjava.java
+++ src/main/java/tempjava.java
@@ -3,7 +3,7 @@ public class tempjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("tempjava");
+System.out.println("############S");
 	}
 
 }
